9d22ccd7c379a926909da830ae376a3ae86096ba
Kai Minato <minatodevstar@gmail.com>
2022-09-01 11:38:16 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jade amber',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
