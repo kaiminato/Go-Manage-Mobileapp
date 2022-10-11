f70659ae588739ee8ed9cb526dd7e3dec9cb5e89
Kai Minato <minatodevstar@gmail.com>
2022-10-11 20:29:25 +0530

    design updated of store functionality

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute} from '@angular/router';
+import { ApiDataService } from '../services/api-data.service';
+import { DataService } from '../services/data.service';
+import { ImageService } from '../services/image.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss'],
+})
+export class ProductDetailComponent implements OnInit {
+
+  HEADING: string = "Product info";
+  PRODUCT_ID: any = '';
+
+  constructor(
+    private router: Router,
+    private apiData: ApiDataService,
+    private dataService: DataService,
+    private activatedRoute: ActivatedRoute,
+    public imageService: ImageService,
+  ) { }
+
+  ngOnInit() {}
+
+  async ionViewWillEnter (){
+
+    this.PRODUCT_ID = this.activatedRoute.snapshot.params['id'];
+    console.log('PRODUCT_ID--' , this.PRODUCT_ID)
+  }
+
+  navigation() {
+
+    console.log('back  button is triggered')
+    this.router.navigate(['/store-all-product']);
+  }
+
+}
