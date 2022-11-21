f6b1cb9f02b849869fd4c3ad8dfd6ee5f62e1c52
Kai Minato <minatodevstar@gmail.com>
2022-11-21 10:58:58 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl : "https://91.250.249.133:4601/",
-  apiUrl: "https://release-sleet.gomanage.dev",
+  apiUrl: "https://release-sleet.gomanage.dev/",
 };
 
 /*
