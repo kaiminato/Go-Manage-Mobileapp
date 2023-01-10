e3df6b5d0e8ec042434c1255c6cfe21412b84e14
Kai Minato <minatodevstar@gmail.com>
2023-01-10 22:43:03 +0000

    adding multi environment files

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import config from '../app/auth_config.json';
+import config from '../app/auth_config_dev.json';
 
 const { domain, clientId, audience, apiUri, errorPath } = config as {
   domain: string;
@@ -18,7 +18,7 @@ export const environment = {
   auth: {
     domain,
     clientId,
-    ...(audience && audience !== 'http://localhost:4301' ? { audience } : null),
+    ...(audience && audience !== 'https://dev-3h98vtcx.us.auth0.com/' ? { audience } : null),
     redirectUri: window.location.origin,
     errorPath,
     apiUri,
