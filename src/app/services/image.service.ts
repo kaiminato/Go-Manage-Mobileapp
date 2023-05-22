e12322048d70d7ba50c3e1be046c6c5bbc4c5ec7
Kai Minato <minatodevstar@gmail.com>
2023-05-22 12:03:42 +0000

    review page

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -17,6 +17,7 @@ export class ImageService {
   public CONTACT_ICON: string = '../assets/images/contact.jpeg';
   public GIFT_IMG: string = '../assets/images/gift.png';
   public GIFT_CARD_IMG: string = '../assets/images/gift-card.png';
+  public  JADE_SIGN_IMG: string = '../assets/images/JadeAmberSign.png';
 
   constructor() { }
 }
