fc14e46a28efbe1095959736e9b31636ce612abc
Kai Minato <minatodevstar@gmail.com>
2023-05-31 16:14:13 +0000

    online-store

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -13,11 +13,14 @@ export class ProductDetailComponent implements OnInit {
 
   HEADING: string = "Product info";
   PRODUCT_ID: any = '';
-  PRODUCT_PRICE: any = '20';
-  PRODUCT_TITLE: string = 'Prodduct title';
-  PRODUCT_SUBTITLE: string = 'Product sub title';
-  PRODUCT_CONTENT: string = 'Establishing who you are and how you communicate your brand voice will connect and build trust with the people within and outside of your company.'
-
+  PRODUCT_IMG: any;
+  PRODUCT_PRICE: any = '';
+  PRODUCT_NAME: string = '';
+  PRODUCT_DESCRIPTION: string = '';
+  PRODUCT_LIST: any = [];
+  PRODUCT_RESPONSE: any = [];
+  PRODUCT_Detail_ARR: any = [];
+  PRODUCT_Detail: any = {};
   constructor(
     private router: Router,
     private apiData: ApiDataService,
@@ -31,9 +34,30 @@ export class ProductDetailComponent implements OnInit {
   async ionViewWillEnter (){
 
     this.PRODUCT_ID = this.activatedRoute.snapshot.params['id'];
-    
+    await this._getProducts();
   }
-
+  async _getProducts() {
+    await this.apiData.presentLoading();
+    await (await this.apiData._getProducts()).subscribe(
+      async (response: any ) => {
+        await this.apiData.dismiss();
+        this.PRODUCT_LIST = response;
+        this.PRODUCT_RESPONSE = response;
+        console.log("this is product list", this.PRODUCT_RESPONSE);
+        this.PRODUCT_Detail_ARR =  await this.PRODUCT_RESPONSE.filter( data => data.id == Number(this.PRODUCT_ID));
+        this.PRODUCT_Detail = this.PRODUCT_Detail_ARR[0];
+        console.log("this is product detail",this.PRODUCT_Detail);
+        this.PRODUCT_IMG = this.PRODUCT_Detail.image;
+        this.PRODUCT_PRICE = this.PRODUCT_Detail.price;
+        this.PRODUCT_NAME = this.PRODUCT_Detail.name;
+        this.PRODUCT_DESCRIPTION = this.PRODUCT_Detail.description;
+      },
+      async (error: any) => {
+        await this.apiData.dismiss();
+        console.log("this is error",error);
+      }
+    );
+  }
   navigation() {
 
     this.router.navigate(['/store-all-product']);
--- src/app/store-all-product/store-all-product.component.ts
+++ src/app/store-all-product/store-all-product.component.ts
@@ -16,29 +16,27 @@ export class StoreAllProductComponent implements OnInit {
   @ViewChild('mySlider')  slides: IonSlides;
 
 
-  HEADING: string = "Select a product";
+  HEADING: string = "Online Store";
   SELECTED_CATEGORY: string = '';
+  SELECTED_CATEGORY_ID: number = 0;
   CATEGORY_TAGS: any =  [
-                          { id: 1 , name: "Men" , status: false },
-                          { id: 2 , name: "WoMen" , status: true },
-                          { id: 3 , name: "Kids" , status: false },
-                          { id: 4 , name: "Test" , status: false },
-                          { id: 5 , name: "Test 1" , status: false },
-                          { id: 6 , name: "Test 2" , status: false },
-                          { id: 6 , name: "" , status: false },
+                          { id: 0 , name: "All" , status: true },
+                          { id: 1 , name: "Shoes" , status: false },
+                          { id: 2 , name: "T-shirt" , status: false },
                         ];
 
-  PRODUCT_LIST: any = [
-                        { id: 1 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 3.5 , price: 40},
-                        { id: 2 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 4.5 , price: 20},
-                        { id: 3 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 5 , price: 70},
-                        { id: 4 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 3.5 , price: 30},
-                        { id: 5 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 2 , price: 20},
-                        { id: 6 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 3.5 , price: 40},
-                      ];
-                      
+  // PRODUCT_LIST: any = [
+  //                       { id: 1 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 3.5 , price: 40},
+  //                       { id: 2 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 4.5 , price: 20},
+  //                       { id: 3 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 5 , price: 70},
+  //                       { id: 4 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 3.5 , price: 30},
+  //                       { id: 5 , image: this.imageService.PRODUCT_ONE , description: 'lorem ipsum' , rating: 2 , price: 20},
+  //                       { id: 6 , image: this.imageService.PRODUCT_TWO , description: 'lorem ipsum' , rating: 3.5 , price: 40},
+  //                     ];
+  PRODUCT_LIST: any = [];
+  PRODUCT_RESPONSE: any = [];
 
-            
+
 
   slideOpts: any = {
     slidesPerView: 5,
@@ -62,8 +60,8 @@ export class StoreAllProductComponent implements OnInit {
 
   async ionViewWillEnter (){
 
-    await this.getSelectedcategory()
-
+    await this.getSelectedcategory();
+    await this._getProducts();
   }
 
   async selectCategory (index : any) {
@@ -71,19 +69,45 @@ export class StoreAllProductComponent implements OnInit {
     this.CATEGORY_TAGS.map( data => data.status = false);
     this.CATEGORY_TAGS[index].status = true;
     this.slides.slideTo(index,2000);
-    await this.getSelectedcategory()
+    await this.getSelectedcategory();
   }
 
   async getSelectedcategory () {
 
     let active_category = this.CATEGORY_TAGS.filter(data => data.status == true);
     this.SELECTED_CATEGORY = active_category.length > 0 ? active_category[0].name : '';
+    this.SELECTED_CATEGORY_ID = active_category.length > 0 ? active_category[0].id : 0;
+    this.PRODUCT_LIST =  this.SELECTED_CATEGORY_ID !=0 ?  await this.PRODUCT_RESPONSE.filter( data => data.brandId == this.SELECTED_CATEGORY_ID) : this.PRODUCT_RESPONSE;
   }
 
 
   navigation() {
 
     this.router.navigate(['/']);
+  }
+
+  async _getProducts() {
+    await this.apiData.presentLoading();
+    await (await this.apiData._getProducts()).subscribe(
+      async (response: any ) => {
+        await this.apiData.dismiss();
+        this.PRODUCT_LIST = response;
+        this.PRODUCT_RESPONSE = response;
+        console.log("this is product list",response);
+      },
+      async (error: any) => {
+        await this.apiData.dismiss();
+        console.log("this is error",error);
+      }
+    );
+  }
+  async onSearch(SEARCH_TEXT: string) {
+      console.log("this is onsearch function",SEARCH_TEXT);
+      this.PRODUCT_LIST = await this.PRODUCT_RESPONSE.filter( data =>
+        ((data.name.toLocaleLowerCase()).indexOf(SEARCH_TEXT.toLocaleLowerCase()) != -1)
+        ||
+        ((data.description.toLocaleLowerCase()).indexOf(SEARCH_TEXT.toLocaleLowerCase()) != -1)
+        );
   }
 
 }
