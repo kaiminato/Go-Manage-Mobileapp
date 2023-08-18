b68a8222fcf9bfe8ff2204d1a870707cca20c23f
Kai Minato <kaiminatodev@gmail.com>
2023-08-18 10:38:30 +0000

    changed some case

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -68,7 +68,7 @@ export class ProfileComponent implements OnInit {
       async (response: any) => {
         console.log("response",response);
         let userEmail;
-        if(response.email){
+        if(response.hasOwnProperty('email')){
           userEmail = response.email;
           console.log("userEmail",userEmail);
         }
