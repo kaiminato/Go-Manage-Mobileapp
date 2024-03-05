ea5e6c19235e1159168fe430df1873b266e3318d
kieran redington <kieranred90@gmail.com>
2024-03-05 07:08:54 +0000

    adding aws details to jade and c10002 env

--- src/environments/environment.prod.c10002.ts
+++ src/environments/environment.prod.c10002.ts
@@ -10,6 +10,10 @@ const { domain, clientId, audience, apiUri, errorPath } = config as {
 
 export const environment = {
   production: true,
+  AWS_ACCESS_KEY: "AKIASUBES4CYHO7XXOP3",
+  AWS_SECRET_KEY: "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",
+  AWS_S3_BUCKET_NAME: "gomanage.image.storage",
+  AWS_REGION: "eu-west-1",
 
   auth: {
     domain,
