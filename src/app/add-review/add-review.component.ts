b5df486cab163d01f25695eeeb99292e7b23cc5b
Kai Minato <kaiminatodev@gmail.com>
2024-06-13 01:15:16 -0700

    Update DT-97

--- src/app/add-review/add-review.component.ts
+++ src/app/add-review/add-review.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiDataService } from '../services/api-data.service';
 import { DataService } from '../services/data.service';
 import { ImageService } from '../services/image.service';
@@ -18,6 +18,11 @@ export class AddReviewComponent implements OnInit {
   user_id: number;
   BOOKINGS_DETAILS: any;
   currentRating: number = 0;
+  STAFF_LIST: any = [];
+  STAFF_ID: any = '';
+  SELECTED_STAFF_DETAIL:any ;
+
+  selectedOption: any;
 
   constructor(
     private router: Router,
@@ -25,22 +30,25 @@ export class AddReviewComponent implements OnInit {
     public imageService: ImageService,
     public dataService: DataService,
     public auth: AuthService,
+    private activateRoute: ActivatedRoute
   ) { }
 
   ngOnInit() { }
   async ionViewWillEnter() {
     this.getUserInfo();
+    this.STAFF_ID = this.activateRoute.snapshot.paramMap.get('id');
     this.BOOKINGS_DETAILS = await this.dataService.getInitialBookingdata();
   }
   ionViewDidEnter() {
     this.myTextarea.setFocus();
+    this.getStaffList();
   }
   async getUserInfo() {
     await this.auth.getUser().subscribe(
       async (response: any) => {
         (await this.apiData.getMyProfile(response.email)).subscribe(
           async (user_info: any) => {
-            this.user_id = user_info.userGMID;
+            this.user_id = user_info.UserGMID;
           });
       },
       async (error: any) => {
@@ -50,19 +58,28 @@ export class AddReviewComponent implements OnInit {
   setRating(rating: number) {
     this.currentRating = rating;
   }
+  onOptionChange() {
+    this.STAFF_ID = this.selectedOption;
+    this.SELECTED_STAFF_DETAIL = this.STAFF_LIST.filter(staff => staff.employee_id == this.STAFF_ID)[0];
+  }
   async addReview() {
     let data = {
       client_id: this.user_id,
-      employeeId: this.BOOKINGS_DETAILS.staff_id,
+      employeeId: this.SELECTED_STAFF_DETAIL.employee_id,
       reviewText: this.REVIEW_TEXT,
       rating: this.currentRating,
     };
+    if((data.reviewText == null || data.reviewText == '') || (data.rating == null || data.rating <= 0)){
+      await this.apiData.presentAlertWithHeader("Failed", "Please try to enter into respective fields.");
+      return 0;
+    }
     await this.apiData.presentLoading();
     (await this.apiData.addReview(data)).subscribe(
       async (response: any) => {
         await this.apiData.dismiss();
         await this.apiData.presentAlertWithHeader("Review Confirmed", "Thanks for your feedback!");
         this.REVIEW_TEXT = "";
+        this.currentRating = 0;
       },
       async (error: any) => {
 
@@ -72,6 +89,28 @@ export class AddReviewComponent implements OnInit {
     );
   }
 
+  async getStaffList() {
+
+    await this.apiData.presentLoading();
+
+    await (await this.apiData.getStaffList()).subscribe(
+      async (response: any) => {
+        await this.apiData.dismiss();
+        if (response.length > 0) {
+          this.STAFF_LIST = response;
+          this.SELECTED_STAFF_DETAIL = this.STAFF_LIST.filter(staff => staff.employee_id == this.STAFF_ID)[0];
+          if(this.SELECTED_STAFF_DETAIL == null){
+            this.SELECTED_STAFF_DETAIL = this.STAFF_LIST[0];
+          }
+          this.selectedOption = this.SELECTED_STAFF_DETAIL.employee_id;
+        }
+      },
+      async (error: any) => {
+        await this.apiData.dismiss();
+        alert(JSON.stringify(error));
+      }
+    );
+  }
 
   navigation() {
     this.router.navigate(['/']);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -88,6 +88,11 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
+    path: 'add-a-review/:id',
+    component: AddReviewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
     path: 'login',
     component: LoginButtonComponent,
     canActivate: [AuthGuard]
