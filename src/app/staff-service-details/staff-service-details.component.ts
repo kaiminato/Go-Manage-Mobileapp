3d7274b51b87104cddb2fa0c55d2ce426af3fafe
Kai Minato <kaiminatodev@gmail.com>
2024-07-26 02:08:57 -0400

    DT-158 : implement booking reschedule without 1 euro.

--- src/app/staff-service-details/staff-service-details.component.ts
+++ src/app/staff-service-details/staff-service-details.component.ts
@@ -200,7 +200,7 @@ export class StaffServiceDetailsComponent implements OnInit {
   }
 
   navigation() {
-    this.router.navigate(['/make-a-booking']);
+    this.router.navigate(['/make-a-booking'],{ queryParams: this.CANCEL_BOOKING_ID == 0 ? {} : { id: this.CANCEL_BOOKING_ID } });
   }
 
 }
